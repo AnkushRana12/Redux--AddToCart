0796b00e5a1bf3a7ba7720be4b92059059c5a13f
AnkushRana12 <ajajsiw1234@gmail.com>
2025-12-11 07:10:09 +0530

    add

--- src/Components/Addtocart.jsx
+++ src/Components/Addtocart.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Addtocart = () => {
-  const cartselector=useSelector((state)=>state.cart.items);
-  console.log(cartselector.length)
-  return (
-    <div>
-      <Link to='/cart' >
-       <div className="cart">
-    <span className="cart-icon">🛒</span>
-    <span className="cart-count">{cartselector.length>0 ? cartselector.length :0}</span>
-  </div></Link>
-    </div>
-  )
-}
+  const cartselector = useSelector((state) => state.cart?.items || []);
 
-export default Addtocart
+  return (
+    <Link to="/cart">
+      <div className="cart">
+        <span className="cart-icon">🛒</span>
+        <span className="cart-count">{cartselector.length}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default Addtocart;
+
